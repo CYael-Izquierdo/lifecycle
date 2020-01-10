5e2a783d897cd0bd3bdaecb2fa4e19d2add8b784
Yael Izquierdo <welowid@gmail.com>
2020-01-09 23:24:44 -0300

    DessertTimer implemented

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -76,6 +77,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
+
+        dessertTimer = DessertTimer()
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -157,16 +160,17 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         Timber.i("onDestroy called")
     }
 
-
-    override fun onStop() {
-        super.onStop()
-        Timber.i("onStop called")
-    }
-
     override fun onStart() {
         super.onStart()
         // Log.i("MainActivity", "onStart called")
+        dessertTimer.startTimer()
         Timber.i("onStart called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        dessertTimer.stopTimer()
+        Timber.i("onStop called")
     }
 
     override fun onResume() {
